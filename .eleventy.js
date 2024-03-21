b383dd866e65f419faa1996192319b9fb4f1989b
Keming He <keminghe.career@gmail.com>
2024-03-20 21:50:31 -0400

    Re-org-ed files, almost done.

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+// ./.eleventy.js
+//
+// Eleventy configuration file.
+module.exports = function(eleventyConfig) {
+  
+  // Copy `images/` to `_site/images/`
+  //eleventyConfig.addPassthroughCopy('images');
+  // Copy `css/` to `_site/css/`
+  eleventyConfig.addPassthroughCopy('css');
+  // Copy `js/` to `_site/js/`
+  eleventyConfig.addPassthroughCopy('js');
+  // Copy `sass/` to `_site/sass/`
+  eleventyConfig.addPassthroughCopy('sass');
+  // Copy `twig/` to `_site/twig/`
+  eleventyConfig.addPassthroughCopy('twig');
+
+  return {
+    dir: {
+      input: '.',
+      output: '_site'
+    },
+    templateFormats: ['html'],
+    passthroughFileCopy: true
+  };
+};
