8c9581a1804762ad6dc769da8ff8c09513454bb1
Keming He <keminghe.career@gmail.com>
2024-03-26 14:12:16 -0400

    Further debug CI.

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
+import Example from "../components/Example";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/example" element={<Example />} />
       </Routes>
     </BrowserRouter>
   );
