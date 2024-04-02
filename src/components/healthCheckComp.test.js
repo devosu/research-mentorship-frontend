f36c0abd8909bc8481763a67ef18dc7d86e7751a
Keming He <keminghe.career@gmail.com>
2024-04-02 15:15:58 -0400

    Revised workflow, added healthcheck and Dockerfile.

---
+++ src/components/healthCheckComp.test.js
@@ -0,0 +1,13 @@
+// Path: <rootDir>/src/components/healthCheckComp.test.js
+//
+// Test file for the HealthCheckComp component.
+import React from "react";
+import HealthCheckComp from "./healthCheckComp";
+import { render, screen } from "@testing-library/react";
+
+describe("Health check component", () => {
+  it("should render", () => {
+    render(<HealthCheckComp />);
+    expect(screen.getByText("App is running and healthy.")).toBeInTheDocument();
+  });
+});
