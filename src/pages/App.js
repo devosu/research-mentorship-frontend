bf68e867d4e95e80baad589fca21cb29099d51a1
trev-org04 <58343522+trev-org04@users.noreply.github.com>
2024-04-08 22:59:39 -0400

    edited dependencies for App.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -8,7 +8,6 @@ import Ziqi from "./Ziqi";
 
 // React essential imports.
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // Import pages.
 import Home from "./Home";
