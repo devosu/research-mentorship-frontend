2f09eb39a8ebc4a1d60df36e416b2229a2dc0fb6
trev-org04 <58343522+trev-org04@users.noreply.github.com>
2024-04-08 23:20:51 -0400

    edited main page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
 import "../static/index.css";
 
 const Home = () => {
-  return <div>Welcome to home page for event feed!</div>;
+  return (
+    <>
+      <div>Welcome to home page for event feed!</div>
+  <a href='/yakob'>Go to Yakob's Page</a><br></br>
+  <a href='/ziqi'>Go to Ziqi's Page</a>
+    </>
+  );
 };
 
 export default Home;
