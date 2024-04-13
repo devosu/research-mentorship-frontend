0f8fee579a126604e59d93c27c0d2a9b53a2c2d0
ZiqiAbbyOu <145293055+ZiqiAbbyOu@users.noreply.github.com>
2024-04-13 18:08:21 -0400

    Potential feature notes

--- src/app/ziqi/page.client.js
+++ src/app/ziqi/page.client.js
@@ -2,30 +2,30 @@ import './ziqi.css';
 import Image from 'next/image'
 import blank_image from './blank_event_image.png'
 import default_club_icon from './default_club_icon.png'
-// import React, {useState} from 'react';
+import React, {useState} from 'react';
 
 /**
  * @returns {ReactElement} The React element.
  */
 export default function Ziqi() {
-  // const [images, setInmages] = useState([blank_image]);
-  // const [currentIndex, setCurrentIndex] = useState(0);
+  const [images, setInmages] = useState([blank_image]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  // const changeImage = (direction) =>{
-  //   setCurrentIndex(prevIndex =>{
-  //     const newIndex = prevIndex + direction;
-  //     return newIndex < 0? images.length-1 : (newIndex % images.length);
-  //   });
-  // }
+  const changeImage = (direction) =>{
+    setCurrentIndex(prevIndex =>{
+      const newIndex = prevIndex + direction;
+      return newIndex < 0? images.length-1 : (newIndex % images.length);
+    });
+  }
 
 
   return (
     <div className = "event-block">
 
       <div className = "image-gallary-block">
-        {/* <button onClick = {() => changeImage(-1)}>Prev</button> */}
+        <button onClick = {() => changeImage(-1)}>Prev</button>
         <Image src = {blank_image} width = {334} height = {221} alt=''/>
-        {/* <button onClick = {() => changeImage(1)}>Next</button> */}
+        <button onClick = {() => changeImage(1)}>Next</button>
       </div>
 
       <div className = "basic-information-block">
